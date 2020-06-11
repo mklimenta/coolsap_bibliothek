5f040ecb701b3b512c1a528e3ad3dde510f80d2f
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-06-11 14:08:09 +0200

    Project Template

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("net.bounceme.monkeyCoolSAP-Bibliothek.controller.View1", {
+
+	});
+});
