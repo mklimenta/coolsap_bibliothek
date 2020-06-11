36b425ce5be79252016793a17478a9dae93c968c
FmRpw4q2L81rFDF1 <allround.email@gmail.com>
2020-06-11 20:16:57 +0200

    Add borrow and return

--- webapp/controller/BookDetails.controller.js
+++ webapp/controller/BookDetails.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel'
-	], function(Controller, JSONModel) {
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel'
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("net.bounceme.monkeyCoolSAP-Bibliothek.controller.BookDetails", {
 
-		onInit: function () {
+		onInit: function() {
 			var sPath = $.sap.getModulePath("net.bounceme.monkeyCoolSAP-Bibliothek", "/model/books.json");
 			var oModel = new sap.ui.model.json.JSONModel(sPath);
 			this.getView().setModel(oModel);
+		},
+
+		onBorrow: function(oEvent) {
+			var oModel = this.getView().getModel();
+			var availablePath = "/books/0/available_count";
+			var lentPath = "/books/0/lent_count";
+			oModel.setProperty(availablePath, oModel.getProperty(availablePath) + 1);
+			oModel.setProperty(lentPath, oModel.getProperty(lentPath) - 1);
+		},
+
+		onReturn: function(oEvent) {
+			var oModel = this.getView().getModel();
+			var availablePath = "/books/0/available_count";
+			var lentPath = "/books/0/lent_count";
+			oModel.setProperty(availablePath, oModel.getProperty(availablePath) - 1);
+		    oModel.setProperty(lentPath, oModel.getProperty(lentPath) + 1);
 		}
 	});
 });
