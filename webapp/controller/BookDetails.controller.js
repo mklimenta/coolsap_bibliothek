7ab7e3d6e12a90ce93c8704168b4a488ea780dfe
FmRpw4q2L81rFDF1 <allround.email@gmail.com>
2020-06-13 00:34:29 +0200

    Fix model updates

--- webapp/controller/BookDetails.controller.js
+++ webapp/controller/BookDetails.controller.js
@@ -1,22 +1,18 @@
 sap.ui.define([
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel',
-		"sap/ui/core/UIComponent"
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel',
+	"sap/ui/core/UIComponent"
 ], function(Controller, JSONModel, UIComponent) {
 	"use strict";
 
 	return Controller.extend("net.bounceme.monkeyCoolSAP-Bibliothek.controller.BookDetails", {
 
 		onInit: function() {
-			var sPath = $.sap.getModulePath("net.bounceme.monkeyCoolSAP-Bibliothek", "/model/books.json");
-			var oModel = new sap.ui.model.json.JSONModel(sPath);
-			this.getView().setModel(oModel);
-
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 
-		_onObjectMatched: function (oEvent) {
+		_onObjectMatched: function(oEvent) {
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").bookId),
 				model: "books"
@@ -24,19 +20,25 @@ sap.ui.define([
 		},
 
 		onBorrow: function(oEvent) {
-			var oModel = this.getView().getModel();
-			var availablePath = "/books/0/available_count";
-			var lentPath = "/books/0/lent_count";
-			oModel.setProperty(availablePath, oModel.getProperty(availablePath) + 1);
-			oModel.setProperty(lentPath, oModel.getProperty(lentPath) - 1);
+			this._updateModel(-1, +1);
 		},
 
 		onReturn: function(oEvent) {
-			var oModel = this.getView().getModel();
-			var availablePath = "/books/0/available_count";
-			var lentPath = "/books/0/lent_count";
-			oModel.setProperty(availablePath, oModel.getProperty(availablePath) - 1);
-		    oModel.setProperty(lentPath, oModel.getProperty(lentPath) + 1);
+			this._updateModel(+1, -1);
+		},
+
+		_updateModel: function(available, lent) {
+			var oModel = this.getOwnerComponent().getModel("books");
+			var elementPath = this.getView().getElementBinding("books").getPath();
+			var availablePath = elementPath + "/available_count";
+			var lentPath = elementPath + "/lent_count";
+			var availableProperty = oModel.getProperty(availablePath);
+			var lentProperty = oModel.getProperty(lentPath);
+			if ((availableProperty + available) >= 0 && (lentProperty + lent) >= 0) {
+				oModel.setProperty(availablePath, availableProperty + available);
+				oModel.setProperty(lentPath, lentProperty + lent);
+				return;
+			}
 		}
 	});
 });
