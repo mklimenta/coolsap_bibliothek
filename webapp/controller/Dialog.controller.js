e26bfab74a9774d1828c800c9e26e7ce5ddbb1b3
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-06-13 02:57:39 +0200

    fixed input

--- webapp/controller/Dialog.controller.js
+++ webapp/controller/Dialog.controller.js
@@ -9,6 +9,20 @@ sap.ui.define([
 				var sPath = $.sap.getModulePath("net.bounceme.monkeyCoolSAP-Bibliothek", "/model/books.json");
 				var oModel = new sap.ui.model.json.JSONModel(sPath);
 				this.getView().setModel(oModel);
+			},
+			onAddBook: function () {
+				var oModel = this.getOwnerComponent().getModel("books");
+				
+				var isbn = sap.ui.getCore().byId("isbn").getValue();
+				var title = sap.ui.getCore().byId("title").getValue();
+				var author = sap.ui.getCore().byId("author").getValue();
+				var edition = sap.ui.getCore().byId("edition").getValue();
+				var publisher = sap.ui.getCore().byId("publisher").getValue();
+				var language = sap.ui.getCore().byId("language").getValue();
+				var category = sap.ui.getCore().byId("category").getValue();
+				var available_count = sap.ui.getCore().byId("available_count").getValue();
+				
+			    oModel.push(isbn, title, author, edition, publisher, language, category, available_count );
 			}
 	});
 });
