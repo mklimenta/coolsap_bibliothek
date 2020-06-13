1a1bd2ba991203ab657d1b6e49fb8e274298ed7d
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-06-13 14:14:55 +0200

    adde new book

--- webapp/controller/BookTable.controller.js
+++ webapp/controller/BookTable.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function(Controller, JSONModel, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/Fragment"
+], function(Controller, JSONModel, Filter, FilterOperator, Fragment) {
 	"use strict";
 
 	return Controller.extend("net.bounceme.monkeyCoolSAP-Bibliothek.controller.BookTable", {
@@ -37,16 +38,55 @@ sap.ui.define([
         	if (!this._oDialog) {
             	this._oDialog = sap.ui.xmlfragment("net.bounceme.monkeyCoolSAP-Bibliothek.view.Dialog", this);
         		this.getView().addDependent(this._oDialog);
-         }
-         return this._oDialog;
-      },
+        	}
+        	return this._oDialog;
+    	},
       
-      onOpenDialog : function () {
-         this._getDialog().open();
-      },
+    	onOpenDialog : function () {
+        	this._getDialog().open();
+    	},
       
-      onCloseDialog : function () {
-         this._getDialog().close();
-      }
+    	onCloseDialog : function () {
+        	this._getDialog().close();
+    	},
+    	
+    	onAddBook: function () {
+				var oModel = this.getOwnerComponent().getModel("books");
+				var isbn = sap.ui.getCore().byId("isbn")._lastValue; 
+				var title = sap.ui.getCore().byId("title")._lastValue; 
+				var author = sap.ui.getCore().byId("author")._lastValue; 
+				var year = sap.ui.getCore().byId("year")._lastValue; 
+				var edition = sap.ui.getCore().byId("edition")._lastValue; 
+				var publisher = sap.ui.getCore().byId("publisher")._lastValue; 
+				var language = sap.ui.getCore().byId("language").getSelectedItem().getText();; 
+				var category = sap.ui.getCore().byId("category").getSelectedItem().getText();; 
+				var available_count = sap.ui.getCore().byId("available_count")._lastValue; 
+			
+				var model = this.getOwnerComponent().getModel("books");
+				var localdata = model.getData();
+				var jsonString = JSON.stringify(localdata);
+				
+				var moreData = [];
+				var oneMoreEntity= {};
+
+				var id = 3;
+				oneMoreEntity['id']= id++;
+				oneMoreEntity['isbn']= isbn;
+				oneMoreEntity['title']= title;
+				oneMoreEntity['author']= author;
+				oneMoreEntity['year']= year;
+				oneMoreEntity['edition']= edition;
+				oneMoreEntity['publisher']= publisher;
+				oneMoreEntity['language']= language;
+				oneMoreEntity['category']= category;
+				oneMoreEntity['available_count']= available_count;
+				oneMoreEntity['lent_count']= 0;
+				
+				model.setData(localdata, true);
+				/* eslint no-console: "error" */
+				console.log(model.getData());
+				
+				this._getDialog().close();
+			}
 	});
 });
