6381f584cd5d2941fbe3d2ec37d05fcf2e3649ad
FmRpw4q2L81rFDF1 <allround.email@gmail.com>
2020-06-23 16:12:09 +0200

    Toggle buttons active state

--- webapp/controller/BookDetails.controller.js
+++ webapp/controller/BookDetails.controller.js
@@ -12,28 +12,27 @@ sap.ui.define([
 		onInit: function() {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
-			
 		},
+		
 
 		_onObjectMatched: function(oEvent) {
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").bookId),
 				model: "books"
 			});
+			this._toggleButtonActive();
 		},
 
 		onBorrow: function(oEvent) {
-			var updated = this._updateModel(-1, +1);
-			if(updated) {
-			    MessageToast.show("Book borrowed");
-			}
+			this._updateModel(-1, +1);
+			this._toggleButtonActive();
+			MessageToast.show("Book borrowed");
 		},
 
 		onReturn: function(oEvent) {
-			var updated = this._updateModel(+1, -1);
-			if(updated) {
-			    MessageToast.show("Book returned");
-			}
+			this._updateModel(+1, -1);
+			this._toggleButtonActive();
+			MessageToast.show("Book returned");
 		},
 
 		_updateModel: function(available, lent) {
@@ -46,11 +45,32 @@ sap.ui.define([
 			if ((availableProperty + available) >= 0 && (lentProperty + lent) >= 0) {
 				oModel.setProperty(availablePath, availableProperty + available);
 				oModel.setProperty(lentPath, lentProperty + lent);
-				return true;
 			}
-			return false;
 		},
 		
+		_toggleButtonActive: function() {
+		    var oModel = this.getOwnerComponent().getModel("books");
+			var elementPath = this.getView().getElementBinding("books").getPath();
+			var availablePath = elementPath + "/available_count";
+			var lentPath = elementPath + "/lent_count";
+			var availableProperty = Number(oModel.getProperty(availablePath));
+			var lentProperty = Number(oModel.getProperty(lentPath));
+			var borrowBtn = this.getView().byId("borrowBtn");
+			var returnBtn = this.getView().byId("returnBtn");
+			
+		    if(availableProperty <= 0) {
+		        borrowBtn.setEnabled(false);
+		        returnBtn.setEnabled(true);
+		    } else
+		    if(lentProperty <= 0) {
+		        borrowBtn.setEnabled(true);
+		        returnBtn.setEnabled(false);
+		    } else {
+		        borrowBtn.setEnabled(true);
+		        returnBtn.setEnabled(true);
+		    }
+		},
+	
 		onEntryPress: function(oEvent){
 			var oItem = oEvent.getSource();
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
